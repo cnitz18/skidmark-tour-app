55d508f551fd6fecf1a1f1982a36b826b9a7df8c
casey <cnitz1802@gmail.com>
2022-12-03 19:10:10 -0600

    trying to resolve warnings

--- src/components/ServerConfig/ServerNamePassword.js
+++ src/components/ServerConfig/ServerNamePassword.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { useState, useEffect } from "react";
 import { Card, Button, Form, Modal } from "react-bootstrap";
