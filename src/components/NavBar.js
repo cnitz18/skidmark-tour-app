af1b78fe3906e94cb63a7a8eb3a41badfd61d0d2
cnitz <cnitz1802@gmail.com>
2024-04-04 12:22:59 -0500

    lots of changes, mostly UI improvements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Nav, Navbar, Container, Col } from 'react-bootstrap'
 import Home from './Home/Home'
 import SessionHistory from './SessionHistory/SessionHistory'
 import NewServerSetupPage from './NewServerSetup/NewServerSetupPage'
@@ -11,12 +11,17 @@ export default function NavBar({ enums, lists }) {
     <Router>
       <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
+            <Col>
               <Navbar.Brand href="/">Skidmark Tour</Navbar.Brand>
+            </Col>
+            <Col></Col>
+            <Col sm>
               <Nav className="me-auto">
                 <Nav.Link href="/" active>Home</Nav.Link>
                 <Nav.Link href="/history" active>History</Nav.Link>
                 <Nav.Link href="/serversetup" active>Server Setup</Nav.Link>
               </Nav>
+            </Col>
           </Container>
       </Navbar>
       <Routes>
--- src/components/NewServerSetup/NewServerUnavailablePage.js
+++ src/components/NewServerSetup/NewServerUnavailablePage.js
@@ -5,7 +5,7 @@ export default function NewServerUnavailablePage() {
         <div class="d-flex align-items-center justify-content-center vh-50">
             <div class="text-center">
                 <h1 class="display-1 fw-bold">404</h1>
-                <p class="fs-3"> <span class="text-danger">Opps!</span> Dedicated Server is not Online </p>
+                <p class="fs-3"> <span class="text-danger">Oops!</span> Dedicated Server is not Online </p>
                 <p class="lead">
                     Contact Casey Nitz to see wtf be goin on
                     </p>
