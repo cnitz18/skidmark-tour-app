79152f59fdd6591c5c54c4b280cfe6ec3712e048
casey <cnitz1802@gmail.com>
2024-10-31 19:36:08 -0500

    missing new files last commit

---
+++ src/utils/Classes/NameMapper.js
@@ -0,0 +1,35 @@
+class NameMapper {
+    static fromTrackId( trackId, tracklist ) {
+        switch(trackId){
+            default:
+                return tracklist?.find((t) => t.id === trackId)?.name.replaceAll('_',' ') ?? "<undefined>"
+        }
+    }
+    static fromVehicleId( vehicleId ){
+
+    }
+    static fromVehicleClassId( vehicleClassId, classList ){
+        console.log('vehicle class:',vehicleClassId,classList)
+        let mappedName = classList?.find((v) => v.value === vehicleClassId)?.name
+        .replaceAll('Cat','Caterham')
+        .replaceAll('F-','Formula ')
+        .replaceAll('_',' ');
+        switch(mappedName){
+            case "GT1":
+            case "GT3":
+            case "GT3 Gen2":
+            case "GT4":
+            case "GT5":
+            case "LMDh":
+            case "RX":
+            case "DPI":
+            case "TSICup":
+                return mappedName;
+            case "GTOpen":
+                return "GT Open";
+            default:
+                return mappedName.replace(/([A-Z])/g, ' $1').trim() ?? "<undefined>";
+        }
+    }
+}
+export default NameMapper;
