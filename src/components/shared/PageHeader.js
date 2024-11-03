cb9e50947d72be347a15ffc7123c1c40f60ef0a5
casey <cnitz1802@gmail.com>
2024-11-03 01:04:14 -0600

    more UI tweaks

---
+++ src/components/shared/PageHeader.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// import logo from "../../assets/skidmark-placeholder.png";
+
+export default function PageHeader({ title }) {
+  return (
+    <header className="py-2 bg-light border-bottom mb-4">
+        <div className="container">
+            <div className="text-center my-3">
+                {/* {
+                  process.env.REACT_APP_ENV === "Skidmark Tour" ?
+                  <div>
+                    <img className="header-logo" src={logo} alt="The Skidmarks" /> 
+                    <br/><br/>    
+                  </div>: <></>
+                } */}
+                {
+                  title ?
+                  <div>     
+                    <h1 className="fw-bolder">{title}</h1>
+                  </div> : <></>
+                }
+            </div>
+        </div>
+    </header>
+  )
+}
