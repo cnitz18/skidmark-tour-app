fd3ff40ab0ab5c90d9f41037c7bcb5d145f1d73e
casey <cnitz1802@gmail.com>
2025-01-15 17:14:15 -0600

    updated League Standings page

--- src/components/Leagues/LeagueDescriptionStandings.js
+++ src/components/Leagues/LeagueDescriptionStandings.js
@@ -1,5 +1,5 @@
-import { Table, TableHead, TableBody, TableRow, TableCell,Switch, FormControlLabel, Stack, IconButton, Collapse } from "@mui/material";
-import { Tooltip, OverlayTrigger, Row, Col, Card } from "react-bootstrap";
+import { Table, TableHead, TableBody, TableRow, TableCell, Stack, IconButton, Collapse } from "@mui/material";
+import { Tooltip, OverlayTrigger } from "react-bootstrap";
 import { LineChart } from '@mui/x-charts/LineChart';
 import { axisClasses } from "@mui/x-charts";
 import { cheerfulFiestaPalette } from '@mui/x-charts/colorPalettes';
@@ -7,6 +7,12 @@ import NameMapper from "../../utils/Classes/NameMapper";
 import { useState, Fragment } from "react";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Box, Typography } from "@mui/material";
+import { BsTrophy, BsFlag, BsStopwatch, BsBarChart, BsAward } from "react-icons/bs";
+import styles from "./LeagueDescriptionStandings.module.css";
+
+const TABLE_HEIGHT = 400;
+const TABLE_MARGIN = 250;
 
 function StandingsRow(props){
     const { ent,arr,i } = props;
@@ -39,69 +45,54 @@ function StandingsRow(props){
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
                     <Collapse in={openRow} timeout="auto" unmountOnExit>
-                        <Card>
-                            <Card.Header>Season Stats</Card.Header>
-                            <Card.Body>
-                                <Row>
-                                <Col>
-                                    <Card>
-                                        <Card.Header>Race Wins</Card.Header>
-                                        <Card.Body>
-                                            <Card.Title>{ent.Wins}</Card.Title>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col>
-                                    <Card>
-                                        <Card.Header>Pole Positions</Card.Header>
-                                        <Card.Body>
-                                            <Card.Title>{ent.Poles}</Card.Title>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col>
-                                    <Card>
-                                        <Card.Header>Podiums</Card.Header>
-                                        <Card.Body>
-                                            <Card.Title>{ent.Podiums}</Card.Title>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col>
-                                    <Card>
-                                        <Card.Header>Fastest Laps</Card.Header>
-                                        <Card.Body>
-                                            <Card.Title>{ent.FastestLaps}</Card.Title>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col>
-                                    <Card>
-                                        <Card.Header>Points Finishes</Card.Header>
-                                        <Card.Body>
-                                            <Card.Title>{ent.PointsFinishes}</Card.Title>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
+                            {/* <Typography variant="h6" gutterBottom component="div">
+                                Season Statistics
+                            </Typography> */}
+                            <Box className={styles.statsGroup}>
+                                <Stack direction="row" spacing={4} className="mt-2 justify-content-center">
+                                    <Box className={styles.statItem}>
+                                        <BsTrophy className="text-warning mb-1" />
+                                        <Typography variant="h6">{ent.Wins}</Typography>
+                                        <Typography variant="body2" color="text.secondary">Wins</Typography>
+                                    </Box>
+                                    <Box className={styles.statItem}>
+                                        <BsFlag className="text-success mb-1" />
+                                        <Typography variant="h6">{ent.Poles}</Typography>
+                                        <Typography variant="body2" color="text.secondary">Poles</Typography>
+                                    </Box>
+                                    <Box className={styles.statItem}>
+                                        <BsAward className="text-info mb-1" />
+                                        <Typography variant="h6">{ent.Podiums}</Typography>
+                                        <Typography variant="body2" color="text.secondary">Podiums</Typography>
+                                    </Box>
+                                    <Box className={styles.statItem}>
+                                        <BsStopwatch className="text-primary mb-1" />
+                                        <Typography variant="h6">{ent.FastestLaps}</Typography>
+                                        <Typography variant="body2" color="text.secondary">Fastest Laps</Typography>
+                                    </Box>
+                                    <Box className={styles.statItem}>
+                                        <BsBarChart className="text-purple mb-1" />
+                                        <Typography variant="h6">{ent.PointsFinishes}</Typography>
+                                        <Typography variant="body2" color="text.secondary">Points Finishes</Typography>
+                                    </Box>
+                                </Stack>
+                            </Box>
                     </Collapse>
                 </TableCell>
-            </TableRow> 
+            </TableRow>
         </Fragment>
     );
 }
 
 const LeagueDescriptionStandings = ({league,tableSeries,leagueDetails,lists}) => {
-    const [isHidden, setIsHidden] = useState(true);
+    const [isHidden] = useState(true);
 
     return (<>
         {
             (league.races?.length && tableSeries) &&
             <Stack>
-                <h4 className="standings-header">Points Race</h4>
                 <LineChart
+                
                     xAxis={[
                         { 
                             data: league.races.map((r) => NameMapper.fromTrackId(r.track,lists["tracks"]?.list)),
@@ -109,8 +100,8 @@ const LeagueDescriptionStandings = ({league,tableSeries,leagueDetails,lists}) =>
                         }
                     ]}
                     series={tableSeries}
-                    height={400}
-                    margin={{ bottom: 100, right: !isHidden ? 250 : 0 }}
+                    height={ isHidden ? TABLE_HEIGHT : TABLE_HEIGHT + TABLE_MARGIN }
+                    margin={{ bottom: !isHidden ? TABLE_MARGIN + 100 : 100, right: 0 }}
                     sx= {{
                         [`.${axisClasses.bottom} .${axisClasses.tickLabel}`]: {
                             transform: "rotateZ(-45deg) translate(-55px, 0px)"
@@ -120,9 +111,8 @@ const LeagueDescriptionStandings = ({league,tableSeries,leagueDetails,lists}) =>
                         legend: {
                             position: {
                                 vertical: 'bottom',
-                                horizontal: 'right',
                             },
-                            direction: 'column',
+                            direction: 'row',
                             itemMarkWidth: 20,
                             itemMarkHeight: 2,
                             markGap: 5,
@@ -133,7 +123,7 @@ const LeagueDescriptionStandings = ({league,tableSeries,leagueDetails,lists}) =>
                     colors={cheerfulFiestaPalette}
                     className="standings-chart"
                 />
-                <div className="legend-checkbox">
+                {/* <div className="legend-checkbox">
                     <FormControlLabel 
                         label="Show Legend"
                         control={
@@ -141,7 +131,8 @@ const LeagueDescriptionStandings = ({league,tableSeries,leagueDetails,lists}) =>
                                 onChange={(event) => setIsHidden(!event.target.checked)} />
                         } 
                         className="legend-checkbox"/>
-                </div>
+                </div> */}
+                {/* <h4 className="standings-header">Points Race</h4> */}
             </Stack>
         }
         {
