c35d8171537d2f5ca30504b7deed300ab2b4f6f9
casey <cnitz1802@gmail.com>
2025-01-15 18:18:53 -0600

    header, footer, and navbar updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <footer className="py-5 bg-dark">
-        <Container><p className="m-0 text-center text-white">Casey Nitz &copy; 2022-24</p></Container>
+    <footer className={styles.footer}>
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <p className={styles.copyright}>Casey Nitz &copy; 2022-25</p>
+          </Col>
+          <Col md={6}>
+            <div className={styles.socialLinks}>
+              <a href="https://github.com/cnitz18" target="_blank" rel="noopener noreferrer">
+                <BsGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/caseynitz/" target="_blank" rel="noopener noreferrer">
+                <BsLinkedin />
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
--- src/components/shared/PageHeader.js
+++ src/components/shared/PageHeader.js
@@ -1,26 +1,19 @@
 import React from 'react'
-// import logo from "../../assets/skidmark-placeholder.png";
+import styles from './PageHeader.module.css'
 
-export default function PageHeader({ title }) {
+export default function PageHeader({ title, subtitle }) {
   return (
-    <header className="py-2 bg-light border-bottom">
-        <div className="container">
-            <div className="text-center my-3">
-                {/* {
-                  process.env.REACT_APP_ENV === "Skidmark Tour" ?
-                  <div>
-                    <img className="header-logo" src={logo} alt="The Skidmarks" /> 
-                    <br/><br/>    
-                  </div>: <></>
-                } */}
-                {
-                  title ?
-                  <div>     
-                    <h1 className="fw-bolder">{title}</h1>
-                  </div> : <></>
-                }
+    <header className={styles.pageHeader}>
+      <div className="container">
+        <div className={styles.headerContent}>
+          {title && (
+            <div className={styles.titleWrapper}>     
+              <h1 className={styles.title}>{title}</h1>
+              {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
             </div>
+          )}
         </div>
+      </div>
     </header>
   )
 }
