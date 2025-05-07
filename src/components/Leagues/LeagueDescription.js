939c91b96886b3d1afeb0454e7927f94d5d9cd61
casey <cnitz1802@gmail.com>
2025-05-07 12:03:36 -0500

    minor tweaks to mobile UI

--- src/components/Leagues/LeagueDescription.js
+++ src/components/Leagues/LeagueDescription.js
@@ -20,7 +20,7 @@ function LeagueDescriptionTabPanel(props) {
         aria-labelledby={`league-tab-${index}`}
         {...other}
       >
-        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        {value === index && <Box>{children}</Box>}
       </div>
     );
 }
